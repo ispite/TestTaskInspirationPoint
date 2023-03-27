543c53733ff7479e7114fe73cfa4abb0be46cf02
Denis Solovyov <infinityx93@gmail.com>
2023-03-27 16:23:45 +0300

    locked screen orientation, preview won't work

--- app/src/main/java/com/example/testtaskinspirationpoint/MainActivity.kt
+++ app/src/main/java/com/example/testtaskinspirationpoint/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.testtaskinspirationpoint
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.testtaskinspirationpoint.ui.theme.TestTaskInspirationPointTheme
 
@@ -18,14 +19,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestTaskInspirationPointTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
+    }
+}
+
+@Composable
+private fun MyApp(modifier: Modifier = Modifier) {
+//    LockScreenOrientation()
+    Surface(modifier, color = MaterialTheme.colors.background) {
+//        LockScreenOrientation()
+        Greeting(name = "Android")
     }
 }
 
@@ -34,10 +39,17 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 640)
 @Composable
 fun DefaultPreview() {
     TestTaskInspirationPointTheme {
-        Greeting("Android")
+        MyApp()
     }
+}
+
+@Composable
+fun LockScreenOrientation() {
+    val context = LocalContext.current
+    val activity = context.findActivity()
+    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 }
