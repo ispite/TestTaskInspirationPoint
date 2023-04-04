a4c8356467da9138a8d363e056cbd401ba74dfdd
Denis Solovyov <infinityx93@gmail.com>
2023-04-04 22:12:05 +0300

    fixed points calculation behaviour

--- app/src/main/java/com/example/testtaskinspirationpoint/data/CompetitionResult.kt
+++ app/src/main/java/com/example/testtaskinspirationpoint/data/CompetitionResult.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-class CompetitionResult(val id: Int, initialResult: Int? = 0) {
+class CompetitionResult(val id: Int, initialResult: Int? = null) {
 
     var competitionResult by mutableStateOf(initialResult)
 }
